fc66f5b8d0752927ecbbf09ccd176beff0ce7b5c
Viktor <122560983+Careless-boop@users.noreply.github.com>
2023-11-14 02:37:56 +0100

    Added name

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React\n(Вчіть реакт)
+          Learn React<br>(Вчіть реакт)
         </a>
       </header>
     </div>
